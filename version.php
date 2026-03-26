e8d49edffe4f694b822c664093fe680a90345fdf
Petr Skoda <petr@mutms.org>
2026-03-26 18:48:20 +0100

    Fix Moodle 5.2 compatibility

--- version.php
+++ version.php
@@ -32,7 +32,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'block_muprog_my';
 $plugin->version = 2026032650;
 $plugin->requires = 2025041400;
-$plugin->supported = [500, 501];
+$plugin->supported = [500, 502];
 
 $plugin->release = 'v5.0.6.03+';
 
